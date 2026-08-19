18e06d5a4d2808a742f2a2a23f969eab983bc4d5
radiolab <48355256+radiolab81@users.noreply.github.com>
2026-08-19 07:15:49 +0000

    Update build_smi_tcp_streamig_dac.sh

--- tx/build_smi_tcp_streamig_dac.sh
+++ tx/build_smi_tcp_streamig_dac.sh
@@ -1,2 +1,5 @@
-gcc -O2 -I/usr/src/linux-headers-6.12.47+rpt-common-rpi/include/ \
- smi_tcp_streaming_dac.c -o smi_tcp_streaming_dac -lpthread
+#gcc -O2 -I/usr/src/linux-headers-6.12.47+rpt-common-rpi/include/ \
+# smi_tcp_streaming_dac.c -o smi_tcp_streaming_dac -lpthread
+
+gcc -O3 -Wall -D_GNU_SOURCE smi_tcp_streaming_dac.c -o smi_tcp_streaming_dac -lpthread
+
